61048e22cccf42abafd9c7fa47d3db9d5b610322
oceane <oceane.gatien.cef@hotmail.fr>
2017-04-25 17:24:37 +0200

    function(serv envoie une fourmis alea sur l'html) test pour la recuperer et la placer aleatoirement

--- public/app.js
+++ public/app.js
@@ -1,1 +1,40 @@
 console.log('hello');
+gererateAnimalsAlea(10,10, 4);
+//RecupererUnAnimals();
+
+
+
+
+
+
+
+function RecupererUnAnimals() {
+	$.ajax({
+
+		url : "http://localhost:3000/getrandomobject",
+		type: "get",
+		//dataType: "html",
+
+
+	}).done(function(res) {
+		console.log(res);
+	});
+}
+
+
+
+function gererateAnimalsAlea(x, y, nbrFourmi){
+	//RecupererUnAnimals();
+	x = Math.round(Math.random()*nbrFourmi);
+	y = Math.round(Math.random()*nbrFourmi);
+	var placement = [x, y];
+	console.log(placement);	
+	var fourmi = {
+		fourmi : RecupererUnAnimals(),
+		x : x,
+		y : y,
+	};
+	console.log(fourmi);
+
+
+}
--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var app = express();
-
 app.use(express.static('public'));
 
 
 
+
+
+var vegetal = {
+	icone : "V",
+	
+};
+var beteHerbivore = {
+	icone : "BH",
+};
+var beteCarnivore = {
+	icone : "BC",
+};
+var cailloux = {
+	icone : "C",
+};
+
+var animals = [vegetal, beteHerbivore, beteCarnivore, cailloux];
+
+	//-- routes
+
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send("hello");
+});
+
+app.get('/getrandomobject', function (req, res) {
+	res.send(animals[Math.round(Math.random()*animals.length)]);
+	//res.send(animals);
 });
 
 app.listen(3000, function () {
